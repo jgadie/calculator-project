f8b2ed8d3871342c8aa5a6ed877e53e4ca924c76
Jasmine Gadie and Megan Lee <megan@>
2018-01-31 20:54:11 -0800

    update to scripts.js calculator code

---
+++ jss/scripts.js
@@ -0,0 +1,9 @@
+var bmi = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter your weight:"));
+var number2 = parseInt(prompt("Enter your height:"));
+var result = divide(number1, number2^-2)
+var output = "Your BMI is " + result + ".";
+alert(output);
